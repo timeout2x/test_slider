3758e90bd9bba6ff8960080c11634cefc4fa0455
timeout <timeout2x@gmail.com>
2013-03-23 15:42:00 +0100

    Added readme information; Added arrows that i've forgot to code before :)

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-test_slider
-===========
-
 Test slider task
+
+Live preview: http://htmlpreview.github.com/?https://github.com/timeout2x/test_slider/blob/master/index.html
+
+
+Used techniques:
+- Require.js (not really needed for project of this scale, just demonstration);
+- Angular.js (MVVC framework, lighten than Backbone.js which would be an overkill here);
+- jQuery (could have done without it also, used just for selector engine);
+- Fade in/out transition used (though it can be easily switched to carousel one);
+- Less - CSS preprocessor;
--- static/js/app.js
+++ static/js/app.js
@@ -3,12 +3,12 @@ define(['angular'], function (angular) {
 
   angular.module('app', [])
     .controller('WeatherListController', ['$scope', '$http', function($scope, $http) {
-      var url = 'http://query.yahooapis.com/v1/public/yql?q=use%20\'http%3A%2F%2Fgithub.com%2Fyql%2Fyql-tables%2Fraw%2Fmaster%2Fweather%2Fweather.bylocation.xml\'%20as%20we%3Bselect%20*%20from%20we%20where%20unit%3D\'c\'%20and%20location%20in(\'moscow\'%2C%20\'opatija\'%2C%20\'london\'%2C%20\'washington\'%2C%20\'paris\')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=JSON_CALLBACK'
+      var url = 'http://query.yahooapis.com/v1/public/yql?q=use%20\'http%3A%2F%2Fgithub.com%2Fyql%2Fyql-tables%2Fraw%2Fmaster%2Fweather%2Fweather.bylocation.xml\'%20as%20we%3Bselect%20*%20from%20we%20where%20unit%3D\'c\'%20and%20location%20in(\'moscow\'%2C%20\'opatija\'%2C%20\'london\'%2C%20\'washington\'%2C%20\'paris\'%2C%20\'melbourne\')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=JSON_CALLBACK'
       , interval, timeout;
 
       $http.jsonp(url).success(function(data) {
         if (data && data.error) {
-          alert('Yahoo returned following error: '+data.error);
+          alert('Yahoo returned following error: '+data.error.description);
           return;
         }
         if (data && data.query && data.query.results && data.query.results.weather && data.query.results.weather.length > 0) {
@@ -60,16 +60,41 @@ define(['angular'], function (angular) {
         el.addClass('active');
         $('#slider').children().eq(el.index()).addClass('active');
       }
-
-      $('#counter').on('click', 'div', function() {
-        showSlide($(this));
-        
-        // Restart interval to give user some time to view slide
+      
+      function restartSlideShow() {
         if (interval) clearInterval(interval);
         if (timeout) clearTimeout(interval);
         timeout = setTimeout(function() {
           startSlideShow();
         }, 5000);
+      }
+
+      $('#counter').on('click', 'div', function() {
+        showSlide($(this));
+        
+        // Restart interval to give user some time to view slide
+        restartSlideShow();
+      });
+      
+      $('#arrow-prev, #arrow-next').click(function() {
+        var active = $('#counter').children('.active');
+        
+        if ($(this).is('#arrow-prev')) {
+          active = active.prev();
+          if (active.length < 1) {
+            active = $('#counter').children().last();
+          }
+        }
+        else {
+          active = active.next();
+          if (active.length < 1) {
+            active = $('#counter').children().first();
+          }
+        }
+        showSlide(active);
+        
+        // Restart interval to give user some time to view slide
+        restartSlideShow();
       });
 
   }]);
